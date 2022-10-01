74778497f74f29299594acc7c88cd40061240999
Nicolay J <nicolay.joyag@perficient.com>
2022-10-01 11:07:45 -0500

    logger ok

--- Logger/app.py
+++ Logger/app.py
@@ -1,5 +1,18 @@
 import logging
+from Logger import create_app
 from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask import Flask, request
+
+
+app = create_app('Logger')
+app_context = app.app_context()
+app_context.push()
+
+cors = CORS(app)
+
+api = Api(app)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -13,8 +26,17 @@ logger.addHandler(file_handler)
 
 alarmasLog = Flask(__name__)
 
-@alarmasLog.route("/")
-def loggerAlarma(alarmaProcesada):
-    return logger.info('',extra=alarmaProcesada)
+class VistaLogger(Resource):
+    def post(self):
+        usuarioID=request.json["usuarioID"]
+        ip=request.remote_addr
+        token=request.json["token"]
+        data = {
+            "usuarioID" : usuarioID,
+            "ip" : ip,
+            "token" : token
+        }
+        return logger.info('', extra=data)
 
 
+api.add_resource(VistaLogger, '/logger')
